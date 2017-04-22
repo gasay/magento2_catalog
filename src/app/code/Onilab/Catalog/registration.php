e0d0cfd9a40be5d3382b47313aa534bbdef91d6c
dmitry <dmitry.gayduk@onilab.com>
2017-04-22 06:11:49 +0300

    Init backbone for module

--- src/app/code/Onilab/Catalog/registration.php
+++ src/app/code/Onilab/Catalog/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Fruition_Category',
+    'Onilab_Catalog',
     __DIR__
 );
